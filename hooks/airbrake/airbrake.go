0677b7a6f0affad41448e3425fa179252566a9e8
Zeal <xujinzheng@gmail.com>
2015-10-18 21:00:43 +0800

    add airbrake support

---
+++ hooks/airbrake/airbrake.go
@@ -0,0 +1,32 @@
+package airbrake
+
+import (
+	"github.com/Sirupsen/logrus"
+	"gopkg.in/gemnasium/logrus-airbrake-hook.v2"
+
+	"github.com/gogap/logrus_mate"
+)
+
+type AirbrakeHookConfig struct {
+	ProjectId int    `json:"project_id"`
+	APIKey    string `json:"api_key"`
+	Env       string `json:"env"`
+}
+
+func init() {
+	logrus_mate.RegisterHook("airbrake", NewAirbrakeHook)
+}
+
+func NewAirbrakeHook(options logrus_mate.Options) (hook logrus.Hook, err error) {
+	conf := AirbrakeHookConfig{}
+	if err = options.ToObject(&conf); err != nil {
+		return
+	}
+
+	hook = airbrake.NewHook(
+		int64(conf.ProjectId),
+		conf.APIKey,
+		conf.Env)
+
+	return
+}
