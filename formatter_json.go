6704ef87a0540328b561bf9679665f2cb5f9cf14
ibradypod <ibradypod@gmail.com>
2021-02-07 19:46:10 +0800

    update json tag's name

--- formatter_json.go
+++ formatter_json.go
@@ -6,10 +6,10 @@ import (
 )
 
 type JSONFormatterConfig struct {
-	TimestampFormat  string `json:"timestamp_format"`
-	EnableHTMLEscape bool   `json:"enable_html_escape"`
-	PrettyPrint      bool   `json:"pretty_print"`
-	DisableTimestamp bool   `json:"enable_timestamp"`
+	TimestampFormat   string `json:"timestamp_format"`
+	DisableHTMLEscape bool   `json:"disable_html_escape"`
+	DisableTimestamp  bool   `json:"disable_timestamp"`
+	PrettyPrint       bool   `json:"pretty_print"`
 }
 
 func init() {
@@ -20,8 +20,8 @@ func NewJSONFormatter(config config.Configuration) (logrus.Formatter, error) {
 	jsonFormatter := &logrus.JSONFormatter{}
 	if config != nil {
 		jsonFormatter.TimestampFormat = config.GetString("timestamp_format", "")
-		jsonFormatter.DisableTimestamp = !config.GetBoolean("enable_timestamp", true)
-		jsonFormatter.DisableHTMLEscape = !config.GetBoolean("enable_html_escape", false)
+		jsonFormatter.DisableTimestamp = config.GetBoolean("disable_timestamp", false)
+		jsonFormatter.DisableHTMLEscape = config.GetBoolean("disable_html_escape", true)
 		jsonFormatter.PrettyPrint = config.GetBoolean("pretty_print", false)
 	}
 	return jsonFormatter, nil
